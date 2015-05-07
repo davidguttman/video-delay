340a5a5c42d3bb0a1855c16d692bb590bce4f06b
David Guttman <david@davidguttman.com>
2015-05-06 18:52:42 -0700

    fiat lux

---
+++ index.js
@@ -0,0 +1,61 @@
+var raf = require('raf')
+var getusermedia = require('getusermedia')
+var attachMediaStream = require('attachmediastream')
+
+var userDelay = window.location.hash.replace(/^#\/?/, '')
+if (!userDelay) {
+  window.location = window.location.href + '#/10'
+  window.location.reload()
+}
+
+var imgBuffer = []
+var delayFinished = false
+var delayTime = 1000 * userDelay
+
+var video = document.createElement('video')
+document.body.appendChild(video)
+video.style.position = 'absolute'
+video.style.top = 0
+video.style.left = 0
+video.style.height = '10%'
+
+var canvas = document.createElement('canvas')
+canvas.width = 640
+canvas.height = 480
+canvas.style.height = '100%'
+document.body.appendChild(canvas)
+
+var bufferCanvas = document.createElement('canvas')
+bufferCanvas.width = 640
+bufferCanvas.height = 480
+
+var ctx = canvas.getContext('2d')
+var ctxBuffer = bufferCanvas.getContext('2d')
+
+var timeStart
+var timeReady = Inf
+
+getusermedia({video: true, audio: false}, function(err, stream) {
+  if (err) return console.error(err)
+
+  attachMediaStream(stream, video)
+  videoLoop()
+
+  setTimeout(function() {
+    delayFinished = true
+  }, delayTime)
+})
+
+function videoLoop () {
+  raf(videoLoop)
+
+  ctxBuffer.drawImage(video, 0, 0)
+  imgBuffer.push(ctxBuffer.getImageData(0, 0, bufferCanvas.width, bufferCanvas.height))
+
+  if (delayFinished) {
+    ctx.putImageData(imgBuffer.shift(), 0, 0)
+  }
+
+}
+
+window.addEventListener('hashchange', window.location.reload.bind(window.location))
